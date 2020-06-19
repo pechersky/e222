942850b7e4fc52ed28f1e456eedb85d20bb9e5d4
Yakov Pechersky <yakov@pechersky.us>
2020-06-19 18:41:15 -0400

    Make matrix defn files agree in structure and lemma names

--- src/lecture01_invertible_matrices_prop.lean
+++ src/lecture01_invertible_matrices_prop.lean
@@ -2,35 +2,49 @@ import data.matrix.basic
 import algebra.invertible
 
 variables (n : ℕ) (R : Type*) [field R]
-variables (A B C D : matrix (fin n) (fin n) R)
 
+section invertible
+variables (A B C D : matrix (fin n) (fin n) R)
 variables {n} {R} 
--- attempt using (*)
+
 @[reducible] def mat_invertible : Prop := A * B = 1 ∧ B * A = 1
 
-@[simp] lemma mat_invertible_def : mat_invertible A B ↔ A * B = 1 ∧ B * A = 1 := iff.rfl
+lemma inv_mat_unique (hB : mat_invertible A B) (hC : mat_invertible A C) : B = C :=
+by rw [<-one_mul B, <-hC.right, mul_assoc, hB.left, mul_one]
+
+@[simp] lemma mat_invertible_iff : mat_invertible A B ↔ A * B = 1 ∧ B * A = 1 := iff.rfl
 
 @[simp] lemma mat_invertible_inv : mat_invertible A B ↔ mat_invertible B A :=
 by split; intros h; simp * at *
 
+section cancel
+
+-- ... not simp to prevent deep stack
+lemma mat_inv_right_cancel_self (h : mat_invertible A B) : A * B = 1 :=
+h.left
+
+-- ... not simp to prevent deep stack
+lemma mat_inv_left_cancel_self (h : mat_invertible A B) : B * A = 1 :=
+h.right
+
 @[simp] lemma mat_inv_left_cancel (h : mat_invertible A B) : A * B * C = C :=
-by simp * at *
+by { rw [h.left, one_mul] }
 
 @[simp] lemma mat_inv_right_cancel (h : mat_invertible A B) : C * A * B = C :=
-by simp [*, matrix.mul_assoc] at *
+by { rw [mul_assoc, h.left, mul_one] }
 
 @[simp] lemma inv_mat_left_cancel (h : mat_invertible A B) : B * A * C = C :=
-by simp * at *
+by { rw [h.right, one_mul] }
 
-@[simp] lemma inv_mat_right_cancel (h : mat_invertible A B) : C * A * B = C :=
-by simp [*, matrix.mul_assoc] at *
+@[simp] lemma inv_mat_right_cancel (h : mat_invertible A B) : C * B * A = C :=
+by { rw [mul_assoc, h.right, mul_one] }
 
 @[simp] lemma mat_mul_right_inj (h : mat_invertible A B) : A * C = A * D ↔ C = D :=
 begin
   split,
   { intros H,
     replace H := congr_arg ((*) B) H,
-    repeat {rwa [<-mul_assoc, inv_mat_left_cancel _ _ _ h] at H} },
+    repeat { rwa [<-mul_assoc, inv_mat_left_cancel _ _ _ h] at H } },
   { exact congr_arg _} 
 end
 
@@ -40,11 +54,11 @@ begin
   { intros H,
     replace H := congr_arg (λ M, M * B) H,
     dsimp only [] at H,
-    repeat {rwa [inv_mat_right_cancel _ _ _ h] at H} },
+    repeat { rwa [mat_inv_right_cancel _ _ _ h] at H } },
   { exact congr_arg _} 
 end
 
-lemma mat_inv_neq_zero (npos : 0 < n) (h : mat_invertible A B) : A ≠ 0 :=
+lemma inv_mat_nonzero (npos : 0 < n) (h : mat_invertible A B) : A ≠ 0 :=
 begin
   intros hA,
   let z : (fin n) := ⟨0, npos⟩,
@@ -54,10 +68,9 @@ begin
   exact h
 end
 
-lemma mat_inv_one_self (n : ℕ) : mat_invertible (1 : matrix (fin n) (fin n) R) 1 := by simp
+end cancel
 
-lemma inv_mat_unique (hB : mat_invertible A B) (hC : mat_invertible A C) : B = C :=
-by { rw <-mat_mul_right_inj _ _ _ _ hB, simp * at * }
+lemma inv_mat_one (n : ℕ) : mat_invertible (1 : matrix (fin n) (fin n) R) 1 := by simp
 
 variables (n)
 instance inv_matrix (h : mat_invertible A B) : invertible A :=
@@ -67,6 +80,8 @@ instance inv_matrix (h : mat_invertible A B) : invertible A :=
   mul_inv_of_self := h.left,
 }
 variables {n}
+
+end invertible
 
 example (A B : matrix (fin 1) (fin 1) R) (z : fin 1) :
         A ≠ 0 ∧ B z z = 1 / (A z z) ↔ mat_invertible A B :=
@@ -87,7 +102,7 @@ begin
       simp [nonzero hA, hB, hz] } },
   { intro h,
     have nonzeroA : A ≠ 0,
-      from mat_inv_neq_zero _ _ (by linarith) h,
+      from inv_mat_nonzero _ _ (by linarith) h,
     split,
     { exact nonzeroA },
     { refine (eq_div_iff (nonzero nonzeroA)).mpr _,
--- src/lecture01_invertible_matrices_subtype.lean
+++ src/lecture01_invertible_matrices_subtype.lean
@@ -1,8 +1,6 @@
 import data.matrix.basic
 import algebra.invertible
 
-open_locale classical
-
 variables (n : Type*) [fintype n] [decidable_eq n] (R : Type*) [field R]
 
 section invertible
@@ -16,6 +14,9 @@ variables {n} {R}
 lemma inv_mat_unique {A B C : matrix n n R} (hB : A * B = 1) (hC : C * A = 1) : B = C :=
 by rw [<-one_mul B, <-hC, mul_assoc, hB, mul_one]
 
+section coe
+variables (A B : invertible_matrix n R)
+
 instance coe_matrix : has_coe (invertible_matrix n R) (matrix n n R) :=
 by delta invertible_matrix; apply_instance
 
@@ -24,26 +25,40 @@ by delta invertible_matrix; apply_instance
 instance coe_fun : has_coe_to_fun (invertible_matrix n R) :=
 ⟨λ _, n -> n -> R, λ A, A.val⟩
 
-@[simp] lemma coe_invertible_fun_eq_self {A : invertible_matrix n R} : A.val = ⇑A := rfl
+@[simp] lemma coe_invertible_fun_eq_self {A : invertible_matrix n R} : ⇑A = A.val := rfl
 
-lemma inv_ext_iff (A B : invertible_matrix n R) : A = B ↔ (∀ i j, A i j = B i j) :=
+lemma inv_ext_iff : A = B ↔ (∀ i j, A i j = B i j) :=
 iff.trans subtype.ext ⟨λ h i j, congr_fun (congr_fun h i) j, matrix.ext⟩
 
-@[ext] lemma inv_ext (A B : invertible_matrix n R) : (∀ i j, A i j = B i j) -> A = B :=
+@[ext] lemma inv_ext : (∀ i j, A i j = B i j) -> A = B :=
 (inv_ext_iff A B).mpr
 
 -- ... proving an existence of an inverse is noncomputable
 -- ... because we do not construct it
-noncomputable def inv_mat (A : invertible_matrix n R) : matrix n n R :=
+noncomputable def inv_mat : matrix n n R :=
 classical.some A.property
 
-@[simp] lemma inv_mat_cancel_self (A : invertible_matrix n R) : inv_mat A * A = 1 :=
+@[simp] lemma inv_mat_cancel_self : inv_mat A * A.val = 1 :=
 (classical.some_spec A.property).2
 
-@[simp] lemma mat_cancel_inv_self (A : invertible_matrix n R) : ↑A * inv_mat A = 1 :=
+@[simp] lemma inv_mat_cancel_self' : inv_mat A * ↑A = 1 :=
+(classical.some_spec A.property).2
+
+@[simp] lemma inv_mat_cancel_self'' : inv_mat A * ⇑A = 1 :=
+(classical.some_spec A.property).2
+
+@[simp] lemma mat_cancel_inv_self : A.val * inv_mat A = 1 :=
 (classical.some_spec A.property).1
 
-lemma inv_mat_has_inv (A : invertible_matrix n R) :
+@[simp] lemma mat_cancel_inv_self' : ↑A * inv_mat A = 1 :=
+(classical.some_spec A.property).1
+
+end coe
+
+section inv_instances
+variables (A B : invertible_matrix n R)
+
+lemma inv_mat_has_inv :
   ∃ (B : matrix n n R), inv_mat A * B = 1 ∧ B * inv_mat A = 1 :=
 begin
   use A.val,
@@ -56,7 +71,7 @@ end
 noncomputable instance inv_mat_inv : has_inv (invertible_matrix n R) :=
 ⟨λ A, ⟨inv_mat A, inv_mat_has_inv _⟩⟩
 
-lemma inv_mat_mul_has_inv (A B : invertible_matrix n R) :
+lemma inv_mat_mul_has_inv  :
   ∃ (C : matrix n n R), (A.val * ↑B) * C = 1 ∧ C * (↑A * ↑B) = 1 :=
 begin
   obtain ⟨Ainv, hA⟩ := A.property,
@@ -74,6 +89,71 @@ instance inv_mat_mul : has_mul (invertible_matrix n R) :=
 instance inv_mat_one : has_one (invertible_matrix n R) :=
 ⟨⟨1, by {use 1, simp}⟩⟩
 
+@[simp] lemma inv_mat_val : ↑(A⁻¹) = inv_mat A := rfl
+@[simp] lemma inv_mat_val' : (A⁻¹).val = inv_mat A := rfl
+@[simp] lemma inv_mat_apply : ⇑(A⁻¹) = inv_mat A := rfl
+
+@[simp] lemma inv_mat_mul_val : ↑(A * B) = matrix.mul A.val B.val := rfl
+@[simp] lemma inv_mat_mul_val' : (A * B).val = matrix.mul A.val B.val := rfl
+@[simp] lemma inv_mat_mul_apply : ⇑(A * B) = matrix.mul A.val B.val := rfl
+
+@[simp] lemma inv_mat_one_val : ↑(1 : invertible_matrix n R) = (1 : matrix n n R) := rfl
+@[simp] lemma inv_mat_one_val' : (1 : invertible_matrix n R).val = (1 : matrix n n R) := rfl
+@[simp] lemma inv_mat_one_apply : ⇑(1 : invertible_matrix n R) = (1 : matrix n n R) := rfl
+
+end inv_instances
+
+section cancel
+variables (A : invertible_matrix n R)
+variables (B C : matrix n n R)
+
+@[simp] lemma mat_inv_eq_inv_mat : A⁻¹.val = inv_mat A := rfl
+
+@[simp] lemma mat_inv_right_cancel_self : A * A⁻¹ = 1 :=
+by { refine subtype.eq _, rw [inv_mat_mul_val', inv_mat_val'], exact mat_cancel_inv_self _, }
+
+@[simp] lemma mat_inv_val_right_cancel_self : A.val * A⁻¹.val = 1 :=
+by simp
+
+@[simp] lemma mat_inv_left_cancel_self : A⁻¹ * A = 1 :=
+by { refine subtype.eq _, rw [inv_mat_mul_val', inv_mat_val'], exact inv_mat_cancel_self _, }
+
+@[simp] lemma mat_inv_val_left_cancel_self : A⁻¹.val * A.val = 1 :=
+by simp
+
+@[simp] lemma mat_inv_left_cancel : ↑A * ↑A⁻¹ * B = B :=
+by simp
+
+@[simp] lemma mat_inv_right_cancel : B * ↑A * ↑A⁻¹ = B :=
+by simp [mul_assoc, -matrix.mul_eq_mul] 
+
+@[simp] lemma inv_mat_left_cancel : ↑A⁻¹ * ↑A * B = B :=
+by simp
+
+@[simp] lemma inv_mat_right_cancel : B * ↑A⁻¹ * ↑A = B :=
+by simp [mul_assoc, -matrix.mul_eq_mul] 
+
+@[simp] lemma mat_mul_right_inj : ↑A * B = ↑A * C ↔ B = C :=
+begin
+  split,
+  { intros H,
+    replace H := congr_arg ((*) ↑A⁻¹) H,
+    repeat { rwa [<-mul_assoc, inv_mat_left_cancel] at H }, },
+  { exact congr_arg _} 
+end
+
+@[simp] lemma mat_mul_left_inj : B * ↑A = C * ↑A ↔ B = C :=
+begin
+  split,
+  { intros H,
+    replace H := congr_arg (λ M, M * ↑A⁻¹) H,
+    dsimp only [] at H,
+    repeat { rwa [mat_inv_right_cancel] at H } },
+  { exact congr_arg _} 
+end
+
+end cancel
+
 lemma inv_mat_nonzero (n : ℕ) (npos : 0 < n) (A : invertible_matrix (fin n) R) : A.val ≠ 0 :=
 begin
   obtain ⟨B, hB⟩ := A.property,
@@ -85,28 +165,12 @@ begin
   exact hB
 end
 
-@[simp] lemma inv_mat_val (A : invertible_matrix n R) : ↑(A⁻¹) = inv_mat A := rfl
-@[simp] lemma inv_mat_val' (A : invertible_matrix n R) : (A⁻¹).val = inv_mat A := rfl
-@[simp] lemma inv_mat_apply (A : invertible_matrix n R) : ⇑(A⁻¹) = inv_mat A := rfl
-
-@[simp] lemma inv_mat_mul_val (A B : invertible_matrix n R) : ↑(A * B) = matrix.mul A.val B.val := rfl
-@[simp] lemma inv_mat_mul_val' (A B : invertible_matrix n R) : (A * B).val = matrix.mul A.val B.val := rfl
-@[simp] lemma inv_mat_mul_apply (A B : invertible_matrix n R) : ⇑(A * B) = matrix.mul A.val B.val := rfl
-
-@[simp] lemma inv_mat_one_val : ↑(1 : invertible_matrix n R) = (1 : matrix n n R) := rfl
-@[simp] lemma inv_mat_one_val' : (1 : invertible_matrix n R).val = (1 : matrix n n R) := rfl
-@[simp] lemma inv_mat_one_apply : ⇑(1 : invertible_matrix n R) = (1 : matrix n n R) := rfl
-
 variables (n)
 noncomputable instance inv_matrix (A : invertible_matrix n R) : invertible A :=
 {
   inv_of := A⁻¹,
-  inv_of_mul_self := by { refine subtype.eq _,
-                          rw [inv_mat_mul_val', inv_mat_val'],
-                          exact inv_mat_cancel_self _, } ,
-  mul_inv_of_self := by { refine subtype.eq _,
-                          rw [inv_mat_mul_val', inv_mat_val'],
-                          exact mat_cancel_inv_self _, }
+  inv_of_mul_self := mat_inv_left_cancel_self A,
+  mul_inv_of_self := mat_inv_right_cancel_self A, 
 }
 
 variables {n}
@@ -124,7 +188,8 @@ begin
     { intros nz,
       refine nonzeroA _,
       ext i j,
-      rw [matrix.zero_val, subsingleton.elim i z, subsingleton.elim j z],
+      rw [matrix.zero_val, subsingleton.elim i z, subsingleton.elim j z,
+          <-coe_invertible_fun_eq_self],
       simp [nz] },
   have hinv: A⁻¹ * A = 1,
     from inv_of_mul_self A, 
