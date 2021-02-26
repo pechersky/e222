d53132e7f14c679ba59918c30fa88e9916b14dc2
Yakov Pechersky <ffxen158@gmail.com>
2021-02-26 14:34:03 -0500

    update mathlib and style

--- src/lecture01.lean
+++ src/lecture01.lean
@@ -1,19 +1,16 @@
 import data.real.basic
 import data.matrix.notation
 import linear_algebra.matrix
-import lecture01_invertible_matrices_prop
 import linear_algebra.determinant
+import group_theory.perm.fin
 
 open_locale big_operators
 
-universes u v
+-- We specialize `matrix` for n × n matrices
+notation `Mn[` α `]` n := matrix (fin n) (fin n) α
 
-/-- We specialize matrix for n × n matrices
-with a reducible definition because it's just a shell defn -/
-@[reducible] def Mn (α : Type u) (n : ℕ) := matrix (fin n) (fin n) α
-
-/-- and a shortcut for Mn(ℝ) -/
-@[reducible] def MnR (n : ℕ) := Mn ℝ n
+-- and a shortcut for Mn(ℝ)
+notation `MnR ` n := Mn[ℝ]n
 
 variables {n : ℕ}
 variables (A : MnR n) (B : MnR n) (C : MnR n)
@@ -21,7 +18,7 @@ variables (A : MnR n) (B : MnR n) (C : MnR n)
 -- Call the collection of n × n matrices as Mₙ(ℝ).
 -- That is a vector space over the real numbers, of dimension n².
 example : finite_dimensional.findim ℝ (MnR n) = n ^ 2 :=
-  by simp [nat.pow_two] -- rw [matrix.findim_matrix, fintype.card_fin, nat.pow_two]
+by simp [pow_two]   -- rw [matrix.findim_matrix, fintype.card_fin, nat.pow_two]
 
 -- The addition law in the vector space of n × n matrices
 example : A + B = λ i j, A i j + B i j :=
@@ -42,61 +39,58 @@ by apply_instance
 
 -- An n × n matrix has some finite basis of cardinality n ^ 2
 example : ∃ (b : finset (MnR n)),
-            (is_basis ℝ (subtype.val : (↑b : set (MnR n)) -> (MnR n)) ∧
+            (is_basis ℝ (subtype.val : (b : set (MnR n)) → (MnR n)) ∧
             (finset.card b = n ^ 2)) :=
 begin
   obtain ⟨s, s_basis⟩ := finite_dimensional.exists_is_basis_finset ℝ (MnR n),
   refine ⟨s, s_basis, _⟩,
-  rw [<-finite_dimensional.findim_eq_card_finset_basis s_basis,
-      matrix.findim_matrix, fintype.card, finset.card_fin, nat.pow_two]
+  rw [←finite_dimensional.findim_eq_card_finset_basis s_basis,
+      matrix.findim_matrix, fintype.card, finset.card_fin, pow_two]
 end
 
 variables {m : ℕ}
 
 -- An m × n matrix has some finite basis of cardinality m * n
 example : ∃ (b : finset (matrix (fin m) (fin n) ℝ)),
-            (is_basis ℝ (subtype.val : (↑b : set (matrix (fin m) (fin n) ℝ)) -> (matrix (fin m) (fin n) ℝ)) ∧
+            (is_basis ℝ
+              (subtype.val : (b : set (matrix (fin m) (fin n) ℝ)) → (matrix (fin m) (fin n) ℝ)) ∧
             (finset.card b = m * n)) :=
 begin
   obtain ⟨s, s_basis⟩ := finite_dimensional.exists_is_basis_finset ℝ (matrix (fin m) (fin n) ℝ),
   refine ⟨s, s_basis, _⟩,
-  rw [<-finite_dimensional.findim_eq_card_finset_basis s_basis,
+  rw [←finite_dimensional.findim_eq_card_finset_basis s_basis,
       matrix.findim_matrix],
   repeat { rw [fintype.card, finset.card_fin] }
 end
 
 -- Multiplication is defined on n x n matrices
-#check (by apply_instance : has_mul (MnR n))
+example : has_mul (MnR n) := by show_term { apply_instance }
 
 -- Multiplication is defined by sum of row elements times column elements
 example : A * B = λ i j, ∑ k , A i k * B k j := rfl
 
--- An n x n matrix represents a linear operator T : ℝⁿ -> ℝⁿ
--- ... vectors in ℝⁿ are represented by functions of (fin n) -> ℝ
+-- An n x n matrix represents a linear operator T : ℝⁿ → ℝⁿ
+-- ... vectors in ℝⁿ are represented by functions of (fin n) → ℝ
 -- ... linear maps are notated by →ₗ with an optional →ₗ[field R] parameter
-example : ∃ (T : (fin n → ℝ) →ₗ fin n → ℝ), T.to_matrix = A :=
-by { use A.to_lin, exact to_lin_to_matrix }
+example : ∃ (T : (fin n → ℝ) →ₗ fin n → ℝ), T.to_matrix' = A :=
+by { use A.to_lin', exact linear_map.to_matrix'_to_lin' _ }
 
-example : MnR n ≃ₗ ((fin n → ℝ) →ₗ fin n → ℝ) := linear_equiv_matrix'.symm
+example : (MnR n) ≃ₗ ((fin n → ℝ) →ₗ fin n → ℝ) := matrix.to_lin'
 
 -- Multiplication of matrices is the composition of transformations
-example : A * B = (A.to_lin.comp B.to_lin).to_matrix :=
-by { rw [<-matrix.mul_to_lin, to_lin_to_matrix], refl }
+example : A * B = (A.to_lin'.comp B.to_lin').to_matrix' :=
+by rw [←matrix.to_lin'_mul, linear_map.to_matrix'_to_lin', matrix.mul_eq_mul]
 
 -- Multiplication of 1 x 1 matrices is commutative
--- ... we provide a coercion definition so we can consider
--- ... a 1 x 1 matrix as if it is a scalar of the underlying field
-instance matrix_coe {n : Type u} [fintype n] [unique n] {R : Type v} :
-                    has_coe (matrix n n R) R :=
-let a := default n in ⟨λ M, M a a⟩
-
-theorem coe_singleton {n : Type u} [fintype n] [unique n] [unique n] {R : Type v}
-                      {M : matrix n n R} {a : n} :
-                      (↑M : R) = M a a :=
-by { rw unique.uniq _ a, refl }
-
--- Multiplication of 1 x 1 matrices is commutative, the actual statement
-example (A : MnR 1) (B : MnR 1) : (A : ℝ) * B = B * A := mul_comm _ _
+example (A : MnR 1) (B : MnR 1) : A * B = B * A :=
+begin
+  ext i j,
+  rw [matrix.mul_eq_mul, matrix.mul_eq_mul, matrix.mul_apply, matrix.mul_apply,
+      finset.sum_congr rfl],
+  intros x _,
+  rw mul_comm,
+  congr
+end
 
 -- Multiplication of 2 x 2 matrices is not necessarily commutative
 example : ∃ (A : MnR 2) (B : MnR 2), A * B ≠ B * A :=
@@ -116,7 +110,7 @@ example (A : matrix (fin m) (fin n) ℝ) : A + 0 = A := add_zero _
 example (A : matrix (fin m) (fin n) ℝ) : 0 + A = A := zero_add _
 
 -- The identity matrix
-example {i j} : (1 : MnR n) i j = if i = j then 1 else 0 := matrix.one_val
+example {i j} : (1 : MnR n) i j = if i = j then 1 else 0 := matrix.one_apply
 
 -- Multiplication by the identity matrix gets anything back
 example : A * 1 = A := mul_one _
@@ -131,97 +125,94 @@ example : A * (B * C) = A * B * C :=
 (mul_assoc _ _ _).symm
 
 -- Proving associativity by composition of transformation
-example : (A * (B * C)) = ((A.to_lin.comp B.to_lin).comp C.to_lin).to_matrix :=
-by { rw [linear_map.comp_assoc, <-matrix.mul_to_lin, <-matrix.mul_to_lin,
-        to_lin_to_matrix], refl }
+example : (A * (B * C)) = ((A.to_lin'.comp B.to_lin').comp C.to_lin').to_matrix' :=
+by { rw [linear_map.comp_assoc, ←matrix.to_lin'_mul, ←matrix.to_lin'_mul,
+         linear_map.to_matrix'_to_lin'], refl }
 
 -- A 1 x 1 matrix is invertible iff the element is nonzero
-example (A : MnR 1) (z : fin 1) :
-        A ≠ 0 ↔ ∃ B, mat_invertible A B :=
+example (A : MnR 1) : A ≠ 0 ↔ is_unit A :=
 begin
-  have hz : default (fin 1) = z,
-    from subsingleton.elim _ _,
-  have nonzero : A ≠ 0 -> A z z ≠ 0,
+  have nonzero : A ≠ 0 → A 0 0 ≠ 0,
     { intros nonzeroA nz,
       refine nonzeroA _,
       ext i j,
-      simp [subsingleton.elim i z, subsingleton.elim j z, nz] },
+      convert nz },
   split,
   { intros hA,
-    use ![![1 / (A z z)]],
-    split;
+    refine ⟨⟨A, ![![1 / (A 0 0)]], _, _⟩, rfl⟩;
     { ext i j,
-      rw [matrix.mul_eq_mul, matrix.mul_val, subsingleton.elim i z, subsingleton.elim j z],
-      simp [nonzero hA, hz] } },
-  { rintros ⟨B, h⟩,
-    exact inv_mat_nonzero _ _ (by linarith) h }
+      rw [matrix.mul_eq_mul, matrix.mul_apply, fin.sum_univ_succ, subsingleton.elim i 0,
+          subsingleton.elim j 0],
+      simp [nonzero hA] } },
+  { rintros ⟨⟨A, B, h, h'⟩, rfl⟩ H,
+    have : A 0 0 * B 0 0 = 1,
+      { convert matrix.ext_iff.mpr h 0 0,
+        rw [matrix.mul_eq_mul, matrix.mul_apply, fin.sum_univ_succ, fin.sum_univ_zero, add_zero] },
+    have hz : A 0 0 = 0 := matrix.ext_iff.mpr H 0 0,
+    rw [hz, zero_mul] at this,
+    exact zero_ne_one this }
 end
 
 -- A 2 x 2 matrix is invertible if the "determinant" is nonzero, without definition of determinants
 example (A : MnR 2) {a b c d} (hA : A = ![![a, b], ![c, d]]) :
-        a * d - b * c ≠ 0 ↔ ∃ B, mat_invertible A B :=
+        a * d - b * c ≠ 0 ↔ is_unit A :=
 begin
   split,
   { intros h,
-    use (1 / (a * d - b * c)) • ![![d, -b], ![-c, a]],
-    split;
-      { ext i j,
-        fin_cases i;
-        fin_cases j;
-        field_simp [hA, h];
-        ring } },
-  { intros h H,
-    rcases h with ⟨B, hAB, hBA⟩,
-    rw [<-matrix.ext_iff, hA, matrix.mul_eq_mul] at hBA,
-    simp [mul_cons, matrix.vec_head, matrix.vec_tail] at hBA,
-    let hBA' := hBA 0 0,
-    let hBA'''' := hBA 0 1,
-    let hBA''' := hBA 1 0,
-    let hBA'' := hBA 1 1,
-    have finne : ((0 : fin 2) ≠ 1) := fin.ne_of_vne zero_ne_one,
-    simp [finne] at hBA' hBA'' hBA''' hBA'''',
+    refine ⟨⟨A, (1 / (a * d - b * c)) • ![![d, -b], ![-c, a]], _, _⟩, rfl⟩;
+    { ext i j,
+      fin_cases i;
+      fin_cases j;
+      field_simp [hA, h];
+      ring } },
+  { rintros ⟨⟨A, B, hAB, hBA⟩, rfl⟩ H,
+    simp only [units.coe_mk] at hA,
+    rw [←matrix.ext_iff, hA, matrix.mul_eq_mul] at hBA hAB,
     classical,
-    by_cases ha : a = 0;
-    by_cases hb : b = 0;
-    by_cases hc : c = 0;
-    by_cases hd : d = 0,
-    -- at least one element of A is zero
-    any_goals
-      { simp [ha, hb, hc, hd, mul_zero, sub_zero, mul_eq_zero] at H,
-        cases H;
-        simp * at * },
-    -- all elements of A nonzero
+    by_cases hn : a ≠ 0 ∧ b ≠ 0 ∧ c ≠ 0 ∧ d ≠ 0,
     { have ha' : a = b * c / d,
-        by { rw eq_div_iff hd, rw <-sub_eq_zero, exact H },
-      change (a * B 0 0 + c * B 0 1 = 1) at hBA',
-      change (b * B 0 0 + d * B 0 1 = 0) at hBA'''',
+        { rwa [eq_div_iff, ←sub_eq_zero],
+          simp [hn] },
+      have hBA' : a * B 0 0 + c * B 0 1 = 1,
+        { convert hBA 0 0 using 2;
+          simp [mul_comm] },
+      have hBA'' : b * B 0 0 + d * B 0 1 = 0,
+        { convert hBA 0 1 using 2;
+          simp [mul_comm] },
       have hBv : B 0 1 = - b / d * B 0 0,
-        { field_simp [hb, hd],
-          rw <-eq_neg_iff_add_eq_zero at hBA'''',
-          simp [hBA'''', mul_comm] },
+        { field_simp [hn],
+          rw ←eq_neg_iff_add_eq_zero at hBA'',
+          simp [hBA'', mul_comm] },
       have hd' : d = 0,
-        { field_simp [ha', ha, hb, hc, hd, hBv] at hBA',
-          rw <-hBA',
+        { field_simp [ha', hn, hBv] at hBA',
+          rw ←hBA',
           ring },
-      contradiction } }
+      simpa [hd'] using hn },
+    { simp only [not_and_distrib, not_not, ne.def] at hn,
+      have v0 : ∀ x, matrix.vec_cons 0 (λ (i : fin 1), 0) x = 0,
+        { intros x,
+          fin_cases x;
+          refl },
+      have h00 := hAB 0 0,
+      have h01 := hAB 0 1,
+      have h11 := hAB 1 1,
+      have h10 := hAB 1 0,
+      have h00' := hBA 0 0,
+      have h01' := hBA 0 1,
+      have h11' := hBA 1 1,
+      have h10' := hBA 1 0,
+      rcases hn with (rfl | rfl | rfl | rfl);
+      { simp only [zero_sub, sub_zero, zero_mul, mul_zero, neg_eq_zero, mul_eq_zero] at H,
+        rcases H with (rfl | rfl);
+        simp [matrix.mul_apply, fin.sum_univ_succ] at h00 h01 h11 h10 h00' h01' h11' h10';
+        assumption } } }
 end
 
-variables (a b c d : ℝ)
-
 -- determinant of a 2 x 2 matrix is a * d - b * c
-example (A : MnR 2) {a b c d} (hA : A = ![![a, b], ![c, d]]) :
+example (A : MnR 2) {a b c d : ℝ} (hA : A = ![![a, b], ![c, d]]) :
         matrix.det A = a * d - b * c :=
-calc
-matrix.det A = _ : by { refl }
-...          = ((1 : units ℤ) * (_ * (_ * 1))) + (((-1 : units ℤ) * (_ * (_ * 1))) + 0) : by { refl }
-...          = _ + (_ + 0) : by { refl }
-...          = a * d - b * c : by { simp [hA], refl }
-
-instance fin_inhabited {n : ℕ} : inhabited (fin (n + 1)) := ⟨0⟩
-
-@[simp] lemma fin.default_eq_zero {n : ℕ} : default (fin (n + 1)) = 0 := rfl
-
-@[simp] lemma fin.default_succ_eq_one {n : ℕ} : fin.succ (default (fin (n + 1))) = 1 := rfl
-
-lemma det_2x2 (M : matrix (fin 2) (fin 2) ℝ) : M.det = M 0 0 * M 1 1 - M 1 0 * M 0 1 :=
-by { simp [matrix.det, fin.sum_univ_succ, fin.prod_univ_succ], }
+begin
+  simp [matrix.det, hA, finset.univ_perm_fin_succ, ←finset.univ_product_univ, finset.sum_product,
+        fin.sum_univ_succ, fin.prod_univ_succ],
+  ring
+end
